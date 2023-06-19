ae98d7d87c765f67a043a27aac555f673c46c060
Atul <atul76544@gmailcom>
2023-06-19 11:40:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from './components/KeyPadComponent';
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState("");
+
+  const onClick = (button) => {
+    if (button === "=") {
+      calculate();
+    } else if (button === "C") {
+      reset();
+    } else if (button === "⌫") {
+      backspace();
+    } else {
+      setResult(result + button);
+    }
+  };
+
+  const calculate = () => {
+    let checkResult = "";
+    if (result.includes("--")) {
+      checkResult = result.replace("--", "+");
+    } else {
+      checkResult = result;
+    }
+
+    try {
+      setResult(String(eval(checkResult)) || "");
+    } catch (e) {
+      setResult("error");
+    }
+  };
+
+  const reset = () => {
+    setResult("");
+  };
+
+  const backspace = () => {
+    setResult(result.slice(0, -1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="calculator-body">
+        <h1>The Calculator App</h1>
+        <ResultComponent result={result} />
+        <KeyPadComponent onClick={onClick} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
